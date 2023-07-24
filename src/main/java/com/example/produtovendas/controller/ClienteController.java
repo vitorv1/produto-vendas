692ee41020c3c651456193b96f921f8c0af96838
VitorVieira <vitorhvvieira@gmail.com>
2023-07-24 09:30:36 -0300

    Arrumando pastas do repositório.

--- src/main/java/com/example/produtovendas/controller/ClienteController.java
+++ src/main/java/com/example/produtovendas/controller/ClienteController.java
@@ -5,10 +5,7 @@ import com.example.produtovendas.repository.ClienteRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/cliente")
@@ -19,5 +16,17 @@ public class ClienteController {
     @PostMapping
     public ResponseEntity<Cliente> criarCliente(@RequestBody Cliente cliente){
         return new ResponseEntity<>(repository.save(cliente), HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Cliente> alterarCliente(@RequestBody Cliente cliente){
+        Cliente cliente1 = repository.getReferenceById(cliente.getId());
+        return new ResponseEntity<>(cliente, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping
+    public ResponseEntity<Cliente> deletarCliente(@RequestBody Cliente cliente){
+        return new ResponseEntity<>(repository.delete(cliente), HttpStatus.ACCEPTED);
     }
 }
